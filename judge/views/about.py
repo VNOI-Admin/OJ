d0bca54f753b83a3d8134b028804f0f59c95ad8c
Le Duy Thuc <leduythuc.cs@gmail.com>
2020-12-29 13:19:53 +0700

    Format style code

--- judge/views/about.py
+++ judge/views/about.py
@@ -1,5 +1,5 @@
-#clone from here: https://github.com/LQDJudge/online-judge/blob/master/judge/views/about.py
-#thanks to LQD team
+# clone from here: https://github.com/LQDJudge/online-judge/blob/master/judge/views/about.py
+# thanks to LQD team
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 
