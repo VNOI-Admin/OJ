c89bd0103e7f4d81e8a0152cf8cf2448dd593862
Le Duy Thuc <leduythuc.cs@gmail.com>
2020-12-29 13:15:05 +0700

    Add About page

---
+++ judge/views/about.py
@@ -0,0 +1,10 @@
+#clone from here: https://github.com/LQDJudge/online-judge/blob/master/judge/views/about.py
+#thanks to LQD team
+from django.shortcuts import render
+from django.utils.translation import gettext as _
+
+
+def about(request):
+    return render(request, 'about/about.html', {
+        'title': _('About'),
+    })
