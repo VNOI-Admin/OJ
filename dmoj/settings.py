00bc0609ba674c45e4c4e911e33c33e0e0b410a6
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-01-05 11:49:56 +0700

    Remove all languages except Vi, En

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -358,23 +358,8 @@ LOCALE_PATHS = [
 ]
 
 LANGUAGES = [
-    ('ca', _('Catalan')),
-    ('de', _('German')),
     ('en', _('English')),
-    ('es', _('Spanish')),
-    ('fr', _('French')),
-    ('hr', _('Croatian')),
-    ('hu', _('Hungarian')),
-    ('ja', _('Japanese')),
-    ('ko', _('Korean')),
-    ('pt', _('Brazilian Portuguese')),
-    ('ro', _('Romanian')),
-    ('ru', _('Russian')),
-    ('sr-latn', _('Serbian (Latin)')),
-    ('tr', _('Turkish')),
     ('vi', _('Vietnamese')),
-    ('zh-hans', _('Simplified Chinese')),
-    ('zh-hant', _('Traditional Chinese')),
 ]
 
 BLEACH_USER_SAFE_TAGS = [
