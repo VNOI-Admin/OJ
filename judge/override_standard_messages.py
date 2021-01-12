0068eb9328ed7b0dc2bb6a3620814262c0ebbe1f
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-01-12 11:24:40 +0700

    Format to follow flake8

--- judge/override_standard_messages.py
+++ judge/override_standard_messages.py
@@ -1,8 +1,15 @@
 from django.utils.translation import gettext as _
 from django.utils.translation import ngettext
+
 # Put the default messages that you want to override here
-ngettext("This password is too short. It must contain at least %(min_length)d character.", "This password is too short. It must contain at least %(min_length)d characters.")
-ngettext("Your password must contain at least %(min_length)d character.", "Your password must contain at least %(min_length)d characters.")
+ngettext(
+    "This password is too short. It must contain at least %(min_length)d character.",
+    "This password is too short. It must contain at least %(min_length)d characters.",
+)
+ngettext(
+    "Your password must contain at least %(min_length)d character.",
+    "Your password must contain at least %(min_length)d characters.",
+)
 _("The password is too similar to the %(verbose_name)s.")
 _("Your password can't be too similar to your other personal information.")
 _("This password is entirely numeric.")
