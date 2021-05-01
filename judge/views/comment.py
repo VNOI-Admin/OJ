6a11f343f2243b8ec1f596e572fba2a57576fec0
hung3a8 <hung3a8@gmail.com>
2021-05-01 11:35:13 +0700

    Fix linting

--- judge/views/comment.py
+++ judge/views/comment.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.db import IntegrityError, transaction
-from django.db.models import F
 from django.forms.models import ModelForm
 from django.http import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import get_object_or_404
