9ca9e26432bbefda90517529484bd01209e0e9b3
hung3a8 <hung3a8@gmail.com>
2021-05-12 15:37:35 +0700

    Fix sorting
    Note: Cannot sort without a second sorter in all_sorts

--- judge/views/user.py
+++ judge/views/user.py
@@ -479,9 +479,9 @@ class ContribList(QueryStringSortMixin, DiggPaginatorMixin, TitleMixin, ListView
     model = Profile
     title = gettext_lazy('Contribution')
     context_object_name = 'contributors'
-    template_name = 'user/contrib_list.html'
+    template_name = 'user/contrib-list.html'
     paginate_by = 100
-    all_sorts = frozenset(('contribution_points'))
+    all_sorts = frozenset(('*', 'contribution_points'))
     default_desc = all_sorts
     default_sort = '-contribution_points'
 
