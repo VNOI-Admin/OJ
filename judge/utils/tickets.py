f5fb430bb8444d9beb936935926325451275447f
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-05-31 07:45:44 +0000

    Make general issue appear in 'Open tickets' section

--- judge/utils/tickets.py
+++ judge/utils/tickets.py
@@ -1,7 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
-from judge.models import Problem
+from judge.models import Problem, GeneralIssue
 
 
 def own_ticket_filter(profile_id):
@@ -10,5 +10,6 @@ def own_ticket_filter(profile_id):
 
 def filter_visible_tickets(queryset, user):
     return queryset.filter(own_ticket_filter(user.profile.id) |
+                           Q(content_type=ContentType.objects.get_for_model(GeneralIssue)) |
                            Q(content_type=ContentType.objects.get_for_model(Problem),
                              object_id__in=Problem.get_editable_problems(user))).distinct()
