ee82b1e33ac11eaefea387ca1e1f426f4e8543cc
Le Bao Hiep <baohiep2013@gmail.com>
2021-08-11 22:22:48 +0700

    Fix help text and CSS

--- judge/forms.py
+++ judge/forms.py
@@ -189,7 +189,8 @@ class ProblemSubmitForm(ModelForm):
 class TagProblemCreateForm(Form):
     problem_url = forms.URLField(max_length=200,
                                  label=_('Problem URL'),
-                                 help_text=_('Full URL to the problem.'),
+                                 help_text=_('Full URL to the problem, '
+                                             'e.g. https://oj.vnoi.info/problem/post'),
                                  widget=forms.TextInput(attrs={'style': 'width:100%'}))
 
     def __init__(self, problem_url=None, *args, **kwargs):
