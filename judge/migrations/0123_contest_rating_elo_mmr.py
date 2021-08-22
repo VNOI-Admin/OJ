66083b4a818659bd5f1c05e2ad6bb3c282fc8c98
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-08-22 16:30:20 +0000

    Merge migrations

--- judge/migrations/0123_contest_rating_elo_mmr.py
+++ judge/migrations/0123_contest_rating_elo_mmr.py
@@ -184,7 +184,7 @@ def rate_elo_mmr(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0122_username_display_override'),
+        ('judge', '0121_per_problem_sub_access_control'),
     ]
 
     operations = [
