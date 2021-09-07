c97d169c9911cfa05388a6ddb5b0431a2f785fa1
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-09-07 16:28:51 +0000

    Filter out general ticket

--- judge/utils/tickets.py
+++ judge/utils/tickets.py
@@ -1,7 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
-from judge.models import GeneralIssue, Problem
+from judge.models import Problem
 
 
 def own_ticket_filter(profile_id):
@@ -10,6 +10,5 @@ def own_ticket_filter(profile_id):
 
 def filter_visible_tickets(queryset, user):
     return queryset.filter(own_ticket_filter(user.profile.id) |
-                           Q(content_type=ContentType.objects.get_for_model(GeneralIssue)) |
                            Q(content_type=ContentType.objects.get_for_model(Problem),
                              object_id__in=Problem.get_editable_problems(user))).distinct()
