cb77d2c0c457e976ce05011fe35eff191d5ad439
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-10-29 04:45:19 +0000

    Allow svg

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -577,7 +577,7 @@ MARTOR_UPLOAD_URL_PREFIX = '/martor'
 
 # Directory under MEDIA_ROOT to use to store image uploaded through martor.
 MARTOR_UPLOAD_MEDIA_DIR = 'martor'
-MARTOR_UPLOAD_SAFE_EXTS = {'.jpg', '.png', '.gif'}
+MARTOR_UPLOAD_SAFE_EXTS = {'.jpg', '.png', '.gif', '.svg'}
 
 PDF_STATEMENT_UPLOAD_URL_PREFIX = '/pdf'
 PDF_STATEMENT_UPLOAD_MEDIA_DIR = 'pdf'
