cbfd1b8683ec21c8e1f7b52e50e435cb5d4847cf
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-11-29 15:53:29 +0000

    Fix CSRF error in contest ranking table

--- judge/views/contests.py
+++ judge/views/contests.py
@@ -797,7 +797,7 @@ class ContestRankingBase(ContestMixin, TitleMixin, DetailView):
     def get_rendered_ranking_table(self):
         users, problems = self.get_ranking_list()
 
-        return loader.render_to_string(self.ranking_table_template_name, {
+        return loader.render_to_string(self.ranking_table_template_name, request=self.request, context={
             'users': users,
             'problems': problems,
             'contest': self.object,
