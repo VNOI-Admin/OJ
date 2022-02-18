4c21890e00b66c0e572864a69f99b949d39e8711
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-02-18 23:42:48 +0000

    Add ticket count for admins

--- judge/template_context.py
+++ judge/template_context.py
@@ -7,7 +7,7 @@ from django.utils.functional import SimpleLazyObject, new_method_proxy
 
 from judge import event_poster as event
 from judge.utils.caniuse import CanIUse, SUPPORT
-from .models import NavigationBar, Profile
+from .models import NavigationBar, Profile, Ticket
 
 
 class FixedSimpleLazyObject(SimpleLazyObject):
@@ -59,13 +59,16 @@ def __nav_tab(path):
 
 def general_info(request):
     path = request.get_full_path()
-    return {
+    context = {
         'nav_tab': FixedSimpleLazyObject(partial(__nav_tab, request.path)),
         'nav_bar': NavigationBar.objects.all(),
         'LOGIN_RETURN_PATH': '' if path.startswith('/accounts/') else path,
         'perms': PermWrapper(request.user),
         'HAS_WEBAUTHN': bool(settings.WEBAUTHN_RP_ID),
     }
+    if request.user.is_superuser:
+        context['ticket_count'] = Ticket.objects.filter(is_open=True).count()
+    return context
 
 
 def site(request):
