f10f43a77c82d9527f01cc155719eb18f4d35580
Le Bao Hiep <baohiep2013@gmail.com>
2022-04-17 05:14:31 +0000

    Create safe function for use in Jinja2 templates
    This should allow marking strings as safe (e.g., links) without using
    filters, which break Django's template message extraction logic.

--- judge/jinja2/format.py
+++ judge/jinja2/format.py
@@ -1,4 +1,5 @@
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from . import registry
 
@@ -6,3 +7,8 @@ from . import registry
 @registry.function
 def bold(text):
     return format_html('<b>{0}</b>', text)
+
+
+@registry.function
+def safe(text):
+    return mark_safe(text)
