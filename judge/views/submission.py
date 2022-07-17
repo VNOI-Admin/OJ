b7c1dac622e7ada21f672b79734e3bbba35ebaa8
Le Bao Hiep <baohiep2013@gmail.com>
2022-07-17 20:34:03 +0700

    Hide time/memory usage in submissions list

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -34,11 +34,12 @@ from judge.utils.views import DiggPaginatorMixin, TitleMixin, add_file_response,
 
 
 def submission_related(queryset):
-    return queryset.select_related('user__user', 'problem', 'language') \
-        .only('id', 'user__user__username', 'user__display_rank', 'user__rating', 'problem__name',
-              'problem__code', 'problem__is_public', 'language__short_name', 'language__key', 'date', 'time', 'memory',
-              'points', 'result', 'status', 'case_points', 'case_total', 'current_testcase', 'contest_object',
-              'locked_after', 'problem__submission_source_visibility_mode', 'user__username_display_override') \
+    return queryset.select_related('user__user', 'user__display_badge', 'problem', 'language') \
+        .only('id', 'user__user__username', 'user__display_rank', 'user__rating', 'problem__name', 'problem__code',
+              'problem__is_public', 'language__short_name', 'language__key', 'language__file_only', 'date', 'time',
+              'memory', 'points', 'result', 'status', 'case_points', 'case_total', 'current_testcase', 'contest_object',
+              'locked_after', 'problem__submission_source_visibility_mode', 'problem__testcase_result_visibility_mode',
+              'user__username_display_override', 'user__display_badge__name', 'user__display_badge__mini') \
         .prefetch_related('contest_object__authors', 'contest_object__curators')
 
 
