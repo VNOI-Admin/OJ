5e11c9957ffcd33829ae6652c4594a8d295b423e
Quantum <quantum2048@gmail.com>
2022-10-30 15:40:50 -0400

    Fix WebAuthnInline has_add_permission signature

--- judge/admin/profile.py
+++ judge/admin/profile.py
@@ -53,7 +53,7 @@ class WebAuthnInline(admin.TabularInline):
     readonly_fields = ('cred_id', 'public_key', 'counter')
     extra = 0
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
 
