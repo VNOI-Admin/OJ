4594c1c376252e6d48baf7bf07181ac41f7a2f6b
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-12-03 16:01:06 +0000

    icpc command: remove branch on git pull

--- judge/management/commands/update_testcase.py
+++ judge/management/commands/update_testcase.py
@@ -20,5 +20,5 @@ class Command(BaseCommand):
 
         if not os.path.exists(icpc_folder):
             raise CommandError(f'Folder {icpc_folder} not found. Make sure to clone the repo to that folder')
-        os.system(f'cd {icpc_folder} && git pull origin master')
+        os.system(f'cd {icpc_folder} && git pull')
         print(create_problem(problem_repo_name, icpc_folder))
