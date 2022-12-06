c0f8164b169a96c7969715cfa12ef2f4cf861fce
Quang-Truong Nguyen <jalsol@pm.me>
2022-12-06 22:56:03 +0700

    Disable public ranking before the contest ends (#282)
    * Disable public ranking before the contest ends
    
    * Rename variables and remove auto migrations
    
    * Rename variables and remove auto migrations
    
    * Revert "Rename variables and remove auto migrations"
    
    This reverts commit dd81a1db8f48f9e86e866483f0ac3c2878f76e51.
    
    * Edit migrations
    
    * Remove redundant info in migrations
    
    * Update info
    
    * Edit migration
    
    * Modify messages

--- judge/models/contest.py
+++ judge/models/contest.py
@@ -191,6 +191,9 @@ class Contest(models.Model):
                                            help_text=_('An optional code to view the contest ranking. '
                                                        'Leave it blank to disable.'),
                                            blank=True, default='', max_length=255)
+    ranking_stop_last_minutes = models.IntegerField(verbose_name=_('ranking stop last minutes'), default=0,
+                                              help_text=_('If set, the public scoreboard will be stopped for the '
+                                                          'last X minutes.'))
 
     @cached_property
     def format_class(self):
