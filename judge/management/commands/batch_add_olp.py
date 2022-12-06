07633716a61452877b65a18affef93b2af31cc38
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-12-06 02:22:22 +0000

    Add command to create olp account

---
+++ judge/management/commands/batch_add_olp.py
@@ -0,0 +1,83 @@
+import csv
+import secrets
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from judge.models import Language, Organization, Profile
+
+ALPHABET = 'abcdefghkqtxyz' + 'abcdefghkqtxyz'.upper() + '23456789'
+def generate_password():
+    return ''.join(secrets.choice(ALPHABET) for _ in range(8))
+
+
+def add_user(username, fullname, password, org):
+    usr = User(username=username, is_active=True)
+    usr.set_password(password)
+    usr.save()
+
+    profile = Profile(user=usr)
+    profile.username_display_override = fullname
+    profile.language = Language.objects.get(key=settings.DEFAULT_USER_LANGUAGE)
+    profile.save()
+    profile.organizations.set([org])
+
+
+def get_org(name):
+    org = Organization.objects.get_or_create(
+        name=name,
+        slug='olp',
+        short_name='olp',
+        is_open=False,
+        is_unlisted=False,
+        )[0]
+    return org
+
+
+class Command(BaseCommand):
+    help = 'batch create users'
+
+    def add_arguments(self, parser):
+        parser.add_argument('input', help='csv file containing username and teamname')
+        parser.add_argument('output', help='where to store output csv file')
+        parser.add_argument('prefix', help='prefix for username')
+
+    def handle(self, *args, **options):
+        fin = open(options['input'], 'r', encoding='utf-8')
+        fout = open(options['output'], 'w', encoding='utf-8', newline='')
+        prefix = options['prefix']
+
+        reader = csv.DictReader(fin)
+
+        writer = csv.DictWriter(fout, fieldnames=['username', 'fullname', 'password'])
+        writer.writeheader()
+
+        counter = {
+            'cd': 0,
+            'ct': 0,
+            'sc': 0,
+            'kc': 0,
+        }
+        for row in reader:
+            league = row['league']
+            cnt = counter[league] + 1
+            counter[league] = cnt
+
+            username = f'{prefix}{league}{cnt}'
+            fullname = row['name']
+            org = get_org(row['org'])
+            password = generate_password()
+
+            add_user(username, fullname, password, org)
+
+            writer.writerow({
+                'username': username,
+                'fullname': fullname,
+                'password': password,
+            })
+
+        print(counter)
+
+        fin.close()
+        fout.close()
