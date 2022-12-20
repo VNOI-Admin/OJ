12e3eb848c293e1961006bc76911e639c8a81d1c
Fher <porrino.fernando@gmail.com>
2022-12-20 21:21:13 +0100

    Allow unicode and nobigmath hints to be used with site data (#2055)

--- judge/models/problem_data.py
+++ judge/models/problem_data.py
@@ -64,6 +64,8 @@ class ProblemData(models.Model):
     feedback = models.TextField(verbose_name=_('init.yml generation feedback'), blank=True)
     checker = models.CharField(max_length=10, verbose_name=_('checker'), choices=CHECKERS, default='standard')
     grader = models.CharField(max_length=30, verbose_name=_('Grader'), choices=GRADERS, default='standard')
+    unicode = models.BooleanField(verbose_name=_('enable unicode'), null=True, blank=True)
+    nobigmath = models.BooleanField(verbose_name=_('disable bigInteger / bigDecimal'), null=True, blank=True)
     checker_args = models.TextField(verbose_name=_('checker arguments'), blank=True,
                                     help_text=_('Checker arguments as a JSON object.'))
 
