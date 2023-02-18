6924c1e99986082d9f7c732fe7a29b25830ed0b0
Quantum <quantum2048@gmail.com>
2023-02-18 01:29:10 -0500

    Fix MariaDB insanity with submission language filters

--- judge/views/submission.py
+++ judge/views/submission.py
@@ -406,7 +406,12 @@ class SubmissionsListBase(DiggPaginatorMixin, TitleMixin, ListView):
                                            Q(contest_object__isnull=True))
 
         if self.selected_languages:
-            queryset = queryset.filter(language__in=Language.objects.filter(key__in=self.selected_languages))
+            # MariaDB can't optimize this subquery for some insane, unknown reason,
+            # so we are forcing an eager evaluation to get the IDs right here.
+            # Otherwise, with multiple language filters, MariaDB refuses to use an index
+            # (or runs the subquery for every submission, which is even more horrifying to think about).
+            queryset = queryset.filter(language__in=list(
+                Language.objects.filter(key__in=self.selected_languages).values_list('id', flat=True)))
         if self.selected_statuses:
             queryset = queryset.filter(Q(result__in=self.selected_statuses) | Q(status__in=self.selected_statuses))
         if self.selected_organization:
