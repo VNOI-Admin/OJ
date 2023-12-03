0de0a478b1053aa0a77025e6c46283a5fc53db7f
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-12-03 14:01:04 +0000

    return unk avatar for gravatar

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -10,16 +10,4 @@ from . import registry
 
 @registry.function
 def gravatar(email, size=80, default=None):
-    if isinstance(email, Profile):
-        if default is None:
-            default = email.mute
-        email = email.user.email
-    elif isinstance(email, AbstractUser):
-        email = email.email
-
-    gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(utf8bytes(email.strip().lower())).hexdigest() + '?'
-    args = {'d': 'identicon', 's': str(size)}
-    if default:
-        args['f'] = 'y'
-    gravatar_url += urlencode(args)
-    return gravatar_url
+    return '/martor/logo/unk.png'
