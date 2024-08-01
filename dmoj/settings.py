4de5890c6ed4710fceae7d96d0c818209833a1ca
Le Bao Hiep <baohiep2013@gmail.com>
2024-08-01 13:05:40 +0700

    Enable judge.middleware.IPBasedAuthMiddleware

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -417,6 +417,7 @@ MIDDLEWARE = (
     'judge.middleware.APIMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'judge.middleware.IPBasedAuthMiddleware',
     'judge.middleware.MiscConfigMiddleware',
     'judge.middleware.DMOJLoginMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
