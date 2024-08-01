0bda8e0cc11def47af3d3260260087f75fb8514f
Le Bao Hiep <baohiep2013@gmail.com>
2024-08-02 00:06:32 +0700

    Fix submit_polygon_solutions

--- judge/management/commands/submit_polygon_solutions.py
+++ judge/management/commands/submit_polygon_solutions.py
@@ -48,38 +48,19 @@ class Command(BaseCommand):
             if source_lang.startswith('cpp'):
                 header = '/*\n' + '\n'.join(comments) + '\n*/\n\n'
                 source_code = header + package.read(source_path).decode('utf-8')
-                language = Language.objects.get(key='CPP20')
+                language = Language.objects.get(key='CPPICPC')
             elif source_lang.startswith('java'):
                 header = '/*\n' + '\n'.join(comments) + '\n*/\n\n'
                 source_code = header + package.read(source_path).decode('utf-8')
                 language = Language.objects.get(key='JAVA')
-            elif source_lang.startswith('pas'):
-                header = '{\n' + '\n'.join(comments) + '\n}\n\n'
-                source_code = header + package.read(source_path).decode('utf-8')
-                language = Language.objects.get(key='PAS')
-            elif source_lang.startswith('python'):
+            elif source_lang.startswith('python.pypy3') or source_lang == 'python.3':
                 header = '"""\n' + '\n'.join(comments) + '\n"""\n\n'
                 source_code = header + package.read(source_path).decode('utf-8')
-                if source_lang == 'python.pypy2':
-                    language = Language.objects.get(key='PYPY')
-                elif source_lang.startswith('python.pypy3'):
-                    language = Language.objects.get(key='PYPY3')
-                elif source_lang == 'python.2':
-                    language = Language.objects.get(key='PY2')
-                else:
-                    language = Language.objects.get(key='PY3')
+                language = Language.objects.get(key='PYPY3')
             elif source_lang.startswith('kotlin'):
                 header = '/*\n' + '\n'.join(comments) + '\n*/\n\n'
                 source_code = header + package.read(source_path).decode('utf-8')
                 language = Language.objects.get(key='KOTLIN')
-            elif source_lang == 'go':
-                header = '/*\n' + '\n'.join(comments) + '\n*/\n\n'
-                source_code = header + package.read(source_path).decode('utf-8')
-                language = Language.objects.get(key='GO')
-            elif source_lang == 'rust':
-                header = '/*\n' + '\n'.join(comments) + '\n*/\n\n'
-                source_code = header + package.read(source_path).decode('utf-8')
-                language = Language.objects.get(key='RUST')
             else:
                 print('Unsupported language', source_lang)
                 continue
