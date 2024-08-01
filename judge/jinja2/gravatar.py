7bf2f5ec144832305862a144f2344ce033b3bb6d
Le Bao Hiep <baohiep2013@gmail.com>
2024-08-01 20:48:01 +0700

    Fix lint

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -1,10 +1,3 @@
-import hashlib
-
-from django.contrib.auth.models import AbstractUser
-from django.utils.http import urlencode
-
-from judge.models import Profile
-from judge.utils.unicode import utf8bytes
 from . import registry
 
 
