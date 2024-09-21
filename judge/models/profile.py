3f6475e366e6c6b4d962491a513b1ba45536c012
Nguyễn Hoàng Phúc <nhplhp2004@gmail.com>
2024-09-21 10:03:48 +0700

    Redo organization class

--- judge/models/profile.py
+++ judge/models/profile.py
@@ -65,6 +65,9 @@ class Organization(models.Model):
                                                        'viewing the organization.'))
     performance_points = models.FloatField(default=0)
     member_count = models.IntegerField(default=0)
+    current_consumed_credit = models.FloatField(default=0, help_text='Total used credit this month')
+    available_credit = models.FloatField(default=0, help_text='Available credits')
+    monthly_credit = models.FloatField(default=0, help_text='Total monthly free credit left')
 
     _pp_table = [pow(settings.VNOJ_ORG_PP_STEP, i) for i in range(settings.VNOJ_ORG_PP_ENTRIES)]
 
@@ -109,6 +112,23 @@ class Organization(models.Model):
 
     def get_users_url(self):
         return reverse('organization_users', args=[self.slug])
+
+    def has_credit_left(self):
+        return self.current_consumed_credit < self.available_credit + self.monthly_credit
+
+    def consume_credit(self, consumed):
+        # reduce credit in monthly credit first
+        # then reduce the left to available credit
+        if self.monthly_credit >= consumed:
+            self.monthly_credit -= consumed
+        else:
+            consumed -= self.monthly_credit
+            self.monthly_credit = 0
+            # if available credit can be negative if we don't enable the monthly credit limitation
+            self.available_credit -= consumed
+
+        self.current_consumed_credit += consumed
+        self.save(update_fields=['monthly_credit', 'available_credit', 'current_consumed_credit'])
 
     class Meta:
         ordering = ['name']
