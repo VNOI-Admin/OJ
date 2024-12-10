0c4bfa17dbe9f1a924f8ec22b015648ab5c821f4
Le Duy Thuc <leduythuc.cs@gmail.com>
2024-12-10 17:02:13 +0000

    auth

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -441,6 +441,7 @@ MIDDLEWARE = (
     'judge.middleware.APIMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'judge.middleware.IPBasedAuthMiddleware',
     'judge.middleware.MiscConfigMiddleware',
     'judge.middleware.DMOJLoginMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
