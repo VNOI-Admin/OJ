7802db47d9b5a08161444c467fa6a8abdfd70b15
Le Bao Hiep <baohiep2013@gmail.com>
2024-12-12 19:47:09 +0700

    Force http

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -424,6 +424,7 @@ INSTALLED_APPS += (
 )
 
 MIDDLEWARE = (
+    'judge.middleware.ForceHTTPMiddleware',
     'judge.middleware.ShortCircuitMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
