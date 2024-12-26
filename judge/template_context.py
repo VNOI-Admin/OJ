7a83cb49f936ada47931e385ecf740d07cb76b86
Nguyen Duc Tuan <nguyenductuan11122006@gmail.com>
2024-12-26 14:12:56 +0700

    Support Custom theme and Add Winter theme

--- judge/template_context.py
+++ judge/template_context.py
@@ -31,6 +31,7 @@ def get_resource(request):
         'DMOJ_SCHEME': scheme,
         'DMOJ_CANONICAL': settings.DMOJ_CANONICAL,
         'DMOJ_SELECT2_THEME': settings.DMOJ_SELECT2_THEME,
+        'TOMCHIENXU_CUSTOM_THEME': getattr(settings, 'TOMCHIENXU_CUSTOM_THEME', False)
     }
 
 
