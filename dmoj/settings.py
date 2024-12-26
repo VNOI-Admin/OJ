046b718a64a9f651c8bb2b3b952a2cf088521f6e
int-y1 <jason_yuen2007@hotmail.com>
2024-12-26 00:33:41 -0500

    Remove martor spellcheck

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -639,7 +639,6 @@ MARTOR_ENABLE_CONFIGS = {
     'mention': 'true',
     'jquery': 'false',
     'living': 'false',
-    'spellcheck': 'false',
     'hljs': 'false',
 }
 MARTOR_MARKDOWNIFY_URL = '/widgets/preview/default'
