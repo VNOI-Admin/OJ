bed36a9950b14981ee7594f526941247113a887c
Le Duy Thuc <leduythuc.cs@gmail.com>
2025-01-26 06:14:53 +0000

    [misc] fix lint

--- judge/tasks/organization.py
+++ judge/tasks/organization.py
@@ -1,17 +1,18 @@
 from datetime import datetime, timedelta
 
+import pytz
 from celery import shared_task
 from django.conf import settings
-import pytz
 
 from judge.models import Organization, OrganizationMonthlyUsage
+
 
 @shared_task
 def organization_monthly_reset():
     # Get first day of last month
     current_time = datetime.now(pytz.utc)
     month_start = (current_time.replace(day=1, hour=0, minute=0, second=0, microsecond=0) -
-                  timedelta(days=1)).replace(day=1)
+                   timedelta(days=1)).replace(day=1)
 
     organizations = Organization.objects.filter(current_consumed_credit__gt=0)
 
@@ -19,12 +20,12 @@ def organization_monthly_reset():
         usage = OrganizationMonthlyUsage(
             organization=org,
             time=month_start,
-            consumed_credit=org.current_consumed_credit
+            consumed_credit=org.current_consumed_credit,
         )
         usage.save()
 
     organizations.update(
         monthly_credit=settings.VNOJ_MONTHLY_FREE_CREDIT,
-        current_consumed_credit=0
+        current_consumed_credit=0,
     )
-    print("Reset monthly credit for all organizations")
+    print('Reset monthly credit for all organizations')
