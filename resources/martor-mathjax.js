b9de4dec6aede55bec8b96e8a911ec6f5528a48a
Le Bao Hiep <baohiep2013@gmail.com>
2025-05-31 14:00:29 +0700

    Update static resources. Enable MathJax color extension

--- resources/martor-mathjax.js
+++ resources/martor-mathjax.js
@@ -19,7 +19,7 @@ jQuery(function ($) {
                         window.MathJax.startup = {typeset: false};
                         $.ajax({
                             type: 'GET',
-                            url: '/static/vnoj/mathjax/tex-chtml.min.js',
+                            url: '/static/vnoj/mathjax/3.2.0/es5/tex-chtml.min.js',
                             dataType: 'script',
                             cache: true,
                             success: update_math
