65ef23873f1d727043c81e9c19c9012cf5732378
Le Bao Hiep <baohiep2013@gmail.com>
2025-05-31 14:00:29 +0700

    Update static resources. Enable MathJax color extension

--- resources/mathjax_config.js
+++ resources/mathjax_config.js
@@ -1,5 +1,14 @@
 window.MathJax = {
+    loader: {
+        load: ['[tex]/color'],
+        paths: {
+            mathjax: '/static/vnoj/mathjax/3.2.0/es5'
+        }
+    },
     tex: {
+        packages: {
+            '[+]': ['color']
+        },
         inlineMath: [
             ['~', '~'],
             ['\\(', '\\)']
