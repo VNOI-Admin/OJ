fbf2ccbc3179e00f75af44988b9459ee71433836
Bao-Hiep Le <baohiep2013@gmail.com>
2025-06-15 11:37:25 +0700

    Change default language to CPP20 (#437)

--- judge/utils/problem_data.py
+++ judge/utils/problem_data.py
@@ -195,7 +195,7 @@ class ProblemDataCompiler(object):
                 init['interactive'] = {
                     'files': file_name,
                     'type': 'testlib',  # Assume that we only use testlib interactor
-                    'lang': 'CPP17',
+                    'lang': 'CPP20',
                 }
                 return
 
