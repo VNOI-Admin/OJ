7382d980d40a37e967518ebbeed7a39ee60c1371
Le Bao Hiep <baohiep2013@gmail.com>
2025-07-15 20:54:00 +0700

    return unk avatar for gravatar

--- judge/jinja2/gravatar.py
+++ judge/jinja2/gravatar.py
@@ -1,25 +1,6 @@
-import hashlib
-
-from django.contrib.auth.models import AbstractUser
-from django.utils.http import urlencode
-
-from judge.models import Profile
-from judge.utils.unicode import utf8bytes
 from . import registry
 
 
 @registry.function
 def gravatar(email, size=80, default=None):
-    if isinstance(email, Profile):
-        if default is None:
-            default = email.mute
-        email = email.user.email
-    elif isinstance(email, AbstractUser):
-        email = email.email
-
-    gravatar_url = 'https://www.gravatar.com/avatar/' + hashlib.md5(utf8bytes(email.strip().lower())).hexdigest() + '?'
-    args = {'d': 'identicon', 's': str(size)}
-    if default:
-        args['f'] = 'y'
-    gravatar_url += urlencode(args)
-    return gravatar_url
+    return '/martor/logo/unk.png'
