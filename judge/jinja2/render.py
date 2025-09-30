2987a8a41fd9f8436f79885671d2ce64384717f9
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2025-09-30 18:43:45 +0700

    feat: jinja custom rendering

--- judge/jinja2/render.py
+++ judge/jinja2/render.py
@@ -1,5 +1,5 @@
 from django.template import (Context, Template as DjangoTemplate, TemplateSyntaxError as DjangoTemplateSyntaxError,
-                             VariableDoesNotExist)
+                             VariableDoesNotExist, engines)
 
 from . import registry
 
@@ -19,9 +19,30 @@ def compile_template(code):
     return t
 
 
+def compile_jinja_template(code):
+    if code in django_cache:
+        return django_cache[code]
+
+    # If this works for re.compile, it works for us too.
+    if len(django_cache) > MAX_CACHE:
+        django_cache.clear()
+
+    backend = engines['backend']
+    t = django_cache[code] = backend.from_string(code)
+    return t
+
+
 @registry.function
 def render_django(template, **context):
     try:
         return compile_template(template).render(Context(context))
     except (VariableDoesNotExist, DjangoTemplateSyntaxError):
+        return 'Error rendering: %r' % template
+
+
+@registry.function
+def render_jinja(template, **context):
+    try:
+        return compile_jinja_template(template).render(context)
+    except Exception:
         return 'Error rendering: %r' % template
