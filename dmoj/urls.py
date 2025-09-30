f32f678d1967f0e8bcfb4e9d5ab69424987e2c6e
Le Duy Thuc <leduythuc.cs@gmail.com>
2025-09-30 03:21:47 +0000

    Allow create user in UI

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -205,6 +205,8 @@ urlpatterns = [
     path('set-theme/', user.set_theme, name='set_theme'),
     path('data/prepare/', user.UserPrepareData.as_view(), name='user_prepare_data'),
     path('data/download/', user.UserDownloadData.as_view(), name='user_download_data'),
+    path('bulk/create/', user.BulkUserCreate.as_view(), name='bulk_user_create'),
+    path('bulk/download/<str:task_id>/', user.BulkUserDownload.as_view(), name='bulk_user_download'),
     path('user/<str:user>', include([
         path('', user.UserAboutPage.as_view(), name='user_page'),
         path('/ban', user.UserBan.as_view(), name='user_ban'),
