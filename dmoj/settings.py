8bd862f3d728d04997bab1a87b29248ce121f22d
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-01-26 16:43:59 +0700

    Add low power mode (#505)
    * Limit pagination in low power mode
    
    * Only allow filter by 1 submission result in low power mode
    
    * Disable the submission pie chart in low power mode
    
    * Exclude large users for submission heat map, improve its query performance
    
    * Don't show too many pages in problem submissions
    
    * Eliminate counting in infinityPaginator
    
    * Use infinity paginator for problem list
    
    * remove unused imports

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -119,6 +119,15 @@ VNOJ_PRICE_PER_HOUR = 50
 
 VNOJ_LONG_QUEUE_ALERT_THRESHOLD = 10
 
+# Low power mode: Optimize queries by limiting data scope for performance
+VNOJ_LOW_POWER_MODE = False
+VNOJ_LOW_POWER_MODE_CONFIG = {
+    # limit the number of submissions pages
+    'max_page': 5,
+    # avoid drawing heat map for users with too many submissions
+    'heat_map_limit': 20_000,
+}
+
 VNOJ_MAGAZINE_TAG_SLUG = None
 
 CELERY_TIMEZONE = 'UTC'
