123e9e22b2958ae6a8489d23492cacfcd264821b
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-02-28 10:39:13 +0700

    WIP remove submission_files_size

--- judge/models/problem_data.py
+++ judge/models/problem_data.py
@@ -93,8 +93,6 @@ class ProblemData(models.Model):
                                    help_text=_('grader arguments as a JSON object'))
     zipfile_size = models.BigIntegerField(verbose_name=_('test data storage size'), default=0,
                                           help_text=_('Size of the test data zip file in bytes.'))
-    submission_files_size = models.BigIntegerField(verbose_name=_('submission files storage size'), default=0,
-                                                   help_text=_('Size of all submission files in bytes.'))
 
     def has_yml(self):
         return problem_data_storage.exists('%s/init.yml' % self.problem.code)
@@ -109,46 +107,6 @@ class ProblemData(models.Model):
                 self.zipfile_size = 0
         else:
             self.zipfile_size = 0
-
-    def update_submission_files_size(self):
-        """Calculate total size of all submission files for this problem."""
-        from django.conf import settings
-        from django.core.files.storage import default_storage
-
-        total_size = 0
-        submission_dir = os.path.join(settings.SUBMISSION_FILE_UPLOAD_MEDIA_DIR, self.problem.code)
-
-        try:
-            # Check if the directory exists
-            if default_storage.exists(submission_dir):
-                # Walk through all subdirectories and files
-                dirs, files = default_storage.listdir(submission_dir)
-
-                # Process files in root directory
-                for filename in files:
-                    file_path = os.path.join(submission_dir, filename)
-                    try:
-                        total_size += default_storage.size(file_path)
-                    except (OSError, IOError):
-                        pass
-
-                # Process subdirectories (user_id directories)
-                for user_dir in dirs:
-                    user_path = os.path.join(submission_dir, user_dir)
-                    try:
-                        _, user_files = default_storage.listdir(user_path)
-                        for filename in user_files:
-                            file_path = os.path.join(user_path, filename)
-                            try:
-                                total_size += default_storage.size(file_path)
-                            except (OSError, IOError):
-                                pass
-                    except (OSError, IOError):
-                        pass
-        except (OSError, IOError):
-            pass
-
-        self.submission_files_size = total_size
 
     def save(self, *args, **kwargs):
         # Update zipfile size before saving
