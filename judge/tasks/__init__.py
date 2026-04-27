717126f2d1c1fd48b576edfd384a0251fd4a3923
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2026-04-28 01:14:33 +0700

    fix: register problem tasks with celery

--- judge/tasks/__init__.py
+++ judge/tasks/__init__.py
@@ -1,6 +1,7 @@
 from judge.tasks.contest import *
 from judge.tasks.demo import *
 from judge.tasks.organization import *
+from judge.tasks.problem import *
 from judge.tasks.submission import *
 from judge.tasks.user import *
 from judge.tasks.webhook import *
