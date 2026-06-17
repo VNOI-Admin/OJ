4bae6202365f0f235648214d3d84caf3e614a34d
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-06-17 12:56:04 +0700

    Update migration

--- judge/migrations/0227_add_submission_heatmap_index.py
+++ judge/migrations/0227_add_submission_heatmap_index.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('judge', '0225_add_pac_result'),
+        ('judge', '0226_organization_quota'),
     ]
 
     operations = [
