cb8b9345020c9aec195d32777f1282bb4556c1db
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-06-25 15:15:10 +0700

    Reduce number of view for user files

--- judge/views/user_files.py
+++ judge/views/user_files.py
@@ -1,137 +1,97 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DeleteView, DetailView, ListView, UpdateView, View
+from django.views.generic import DetailView, ListView, View
 
-from judge.forms import UserFileEditForm
 from judge.models import FileAttachment, UserFile
 from judge.utils.user_file_access import authorize_file_access, serve_user_file
 from judge.utils.views import TitleMixin
 
 __all__ = [
-    'UserFileListView', 'UserFileDetailView',
-    'UserFileEditView', 'UserFileDeleteView', 'UserFileBulkDeleteView',
-    'UserFileAccessView',
-    'UserFileSearchView', 'AttachmentAccessView',
+    'UserFileListView', 'UserFileDetailView', 'UserFileDeleteView',
+    'UserFileAccessView', 'UserFileSearchView', 'AttachmentAccessView',
 ]
 
 
-class UserFileBaseMixin(TitleMixin):
+class UserFileMixin(TitleMixin, LoginRequiredMixin):
     model = UserFile
     slug_field = 'uuid'
     slug_url_kwarg = 'uuid'
     context_object_name = 'file'
 
-    def get_queryset(self):
-        return UserFile.objects.none()
 
+class UserFileListView(UserFileMixin, ListView):
+    template_name = 'user/files/file_list.html'
+    context_object_name = 'files'
+    paginate_by = 20
+    title = _('User files')
 
-class UserFilePermissionMixin(UserFileBaseMixin):
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('login')
+        target = get_object_or_404(User, username=kwargs['username'])
+        if not request.user.is_superuser and request.user != target:
+            raise PermissionDenied()
+        self.target_user = target
         return super().dispatch(request, *args, **kwargs)
 
-
-class UserOwnedFilesMixin(UserFilePermissionMixin):
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return UserFile.objects.all()
-        return UserFile.objects.filter(user=self.request.profile)
+        return UserFile.objects.filter(user=self.target_user.profile).order_by('-uploaded_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['target_user'] = self.target_user
+        return context
 
 
-class PublicAccessMixin(UserFileBaseMixin):
+class UserFileDetailView(UserFileMixin, DetailView):
+    template_name = 'user/files/file_detail.html'
+
     def get_object(self, queryset=None):
-        uuid_value = self.kwargs.get(self.slug_url_kwarg)
-        try:
-            file_obj = UserFile.objects.get(uuid=uuid_value)
-        except UserFile.DoesNotExist:
-            raise Http404('File not found or access denied.')
-
-        return authorize_file_access(self.request, file_obj)
-
-
-class UserFileListView(UserOwnedFilesMixin, ListView):
-    title = 'My Files'
-    template_name = 'user/files/file_list.html'
-    paginate_by = 20
-    context_object_name = 'files'
-
-    def get_queryset(self):
-        return super().get_queryset().order_by('-uploaded_at')
+        file_obj = super().get_object(queryset)
+        authorize_file_access(self.request, file_obj)
+        return file_obj
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({
-            'can_edit': True,
-            'can_delete': True,
-        })
+        context['can_delete'] = self.object.can_delete_by(self.request.user)
         return context
 
-
-class UserFileDetailView(PublicAccessMixin, DetailView):
-    title = 'File Details'
-    template_name = 'user/files/file_detail.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        can_edit = self.object.can_change_by(self.request.user)
-        context.update({
-            'can_edit': can_edit,
-            'can_delete': self.object.can_delete_by(self.request.user),
-        })
-        if can_edit:
-            context['form'] = UserFileEditForm(instance=self.object)
-        return context
+    def get_title(self):
+        return self.object.filename
 
 
-class UserFileEditView(UserOwnedFilesMixin, UpdateView):
-    form_class = UserFileEditForm
-    template_name = 'user/files/file_detail.html'
+class UserFileDeleteView(LoginRequiredMixin, View):
+    """POST-only: delete one or more files by UUID. Single delete reuses this endpoint."""
 
-    def get_success_url(self):
-        return reverse_lazy('user_file_detail', kwargs={'uuid': self.object.uuid})
-
-
-class UserFileDeleteView(UserOwnedFilesMixin, DeleteView):
-    title = 'Delete File'
-    template_name = 'user/files/file_delete.html'
-    success_url = reverse_lazy('user_file_list')
-
-
-class UserFileBulkDeleteView(UserFilePermissionMixin, View):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         uuids = request.POST.getlist('uuids')
+        list_url = reverse('user_file_list', args=[request.user.username])
         if not uuids:
             messages.error(request, _('No files selected.'))
-            return redirect('user_file_list')
+            return redirect(list_url)
 
-        qs = UserFile.objects.filter(uuid__in=uuids)
-        if not request.user.is_superuser:
-            qs = qs.filter(user=request.profile)
+        qs = UserFile.objects.filter(uuid__in=uuids, user=request.profile)
 
-        count, __ = qs.delete()
-        messages.success(
-            request,
-            _('%(count)d file(s) deleted.') % {'count': count},
-        )
-        return redirect('user_file_list')
+        count = qs.delete()[0]
+        messages.success(request, _('%(count)d file(s) deleted.') % {'count': count})
+        return redirect(list_url)
 
 
-class UserFileAccessView(PublicAccessMixin, DetailView):
-    template_name = None
-
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        return serve_user_file(request, self.object)
+class UserFileAccessView(LoginRequiredMixin, View):
+    def get(self, request, uuid):
+        try:
+            file_obj = UserFile.objects.get(uuid=uuid)
+        except UserFile.DoesNotExist:
+            raise Http404
+        authorize_file_access(request, file_obj)
+        return serve_user_file(request, file_obj)
 
 
 class UserFileSearchView(LoginRequiredMixin, View):
-    """JSON endpoint for Select2: search the current user's uploaded files."""
-
     def get(self, request):
         q = request.GET.get('q', '')
         qs = UserFile.objects.filter(
@@ -140,16 +100,13 @@ class UserFileSearchView(LoginRequiredMixin, View):
         ).order_by('-uploaded_at')
         if q:
             qs = qs.filter(filename__icontains=q)
-        results = [
+        return JsonResponse({'results': [
             {'id': f.id, 'text': f.filename, 'size': f.size}
             for f in qs[:50]
-        ]
-        return JsonResponse({'results': results})
+        ]})
 
 
 class AttachmentAccessView(View):
-    """Serve a FileAttachment if the requesting user can view its parent object."""
-
     def get(self, request, pk):
         attachment = get_object_or_404(
             FileAttachment.objects.select_related('file', 'content_type'),
@@ -157,5 +114,4 @@ class AttachmentAccessView(View):
         )
         if not attachment.can_view_by(request.user):
             raise Http404
-
         return serve_user_file(request, attachment.file)
