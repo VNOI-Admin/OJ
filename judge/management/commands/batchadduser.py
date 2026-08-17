6e6048f090f7bd2a2724b98547def277e75c0b3e
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-08-17 20:04:17 +0700

    improve batch add user

--- judge/management/commands/batchadduser.py
+++ judge/management/commands/batchadduser.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
-from judge.models import Language, Profile
+from judge.models import Language, Profile, Organization
 
 ALPHABET = 'abcdefghkqtxyz' + 'abcdefghkqtxyz'.upper() + '23456789'
 
@@ -14,14 +14,35 @@ def generate_password():
     return ''.join(secrets.choice(ALPHABET) for _ in range(8))
 
 
-def add_user(username, fullname, password):
-    usr = User(username=username, first_name=fullname, is_active=True)
+def add_user(username, fullname, password, username_display=None, org=None, internal_id=None):
+    usr = User(username=username, is_active=True)
     usr.set_password(password)
     usr.save()
 
     profile = Profile(user=usr)
+
+    if username_display:
+        profile.username_display_override = username_display
+
     profile.language = Language.objects.get(key=settings.DEFAULT_USER_LANGUAGE)
+    profile.site_theme = 'light'
+    if internal_id:
+        profile.notes = internal_id  # save the internal id for later use.
     profile.save()
+    if org:
+        profile.organizations.set([org])
+
+
+def get_org(name):
+    org_id = abs(hash(name) % 1000000007)
+
+    org = Organization.objects.get_or_create(
+        name=name,
+        slug='org' + str(org_id),
+        short_name='org' + str(org_id),
+        is_open=False,
+        is_unlisted=False)[0]
+    return org
 
 
 class Command(BaseCommand):
@@ -39,12 +60,19 @@ class Command(BaseCommand):
         writer = csv.DictWriter(fout, fieldnames=['username', 'fullname', 'password'])
         writer.writeheader()
 
+        has_org = 'organization' in reader.fieldnames
+        has_internal_id = 'internal_id' in reader.fieldnames
+        has_username_display = 'username_display' in reader.fieldnames
+
         for row in reader:
             username = row['username']
             fullname = row['fullname']
+            org = get_org(row['organization']) if has_org else None
+            internal_id = row['internal_id'] if has_internal_id else None
+            username_display = row['username_display'] if has_username_display else None
             password = generate_password()
 
-            add_user(username, fullname, password)
+            add_user(username, fullname, password, username_display=username_display, org=org, internal_id=internal_id)
 
             writer.writerow({
                 'username': username,
