91b73c5a9a777a5ce509491001bb1a2999c344b3
Le Duy Thuc <leduythuc.cs@gmail.com>
2026-08-23 15:09:42 +0700

    Add organization notice banner for org admins (#695)
    * add organization notice
    
    * Update organization logic

--- judge/models/profile.py
+++ judge/models/profile.py
@@ -51,6 +51,8 @@ class Organization(models.Model):
     short_name = models.CharField(max_length=20, verbose_name=_('short name'),
                                   help_text=_('Displayed beside user name during contests.'))
     about = models.TextField(verbose_name=_('organization description'))
+    notice = models.TextField(verbose_name=_('organization notice'), blank=True, default='',
+                              help_text=_('Warning banner shown to the administrators of this organization.'))
     admins = models.ManyToManyField('Profile', verbose_name=_('administrators'), related_name='admin_of',
                                     help_text=_('Those who can edit this organization.'))
     creation_date = models.DateTimeField(verbose_name=_('creation date'), auto_now_add=True)
@@ -104,6 +106,10 @@ class Organization(models.Model):
 
     def is_admin(self, user):
         return user in self.admins_list
+
+    @cached_property
+    def notice_hash(self):
+        return hashlib.sha256(utf8bytes(self.notice)).hexdigest()[:16]
 
     def __contains__(self, item):
         if item is None:
